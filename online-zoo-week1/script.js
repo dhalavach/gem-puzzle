d374845a324f9093bd1e59258c46d5079e711d79
Dzmitry Halavach <kotik1@protonmail.com>
2022-10-08 18:08:15 +0300

    fix: slider nav in pets section

--- online-zoo-week1/script.js
+++ online-zoo-week1/script.js
@@ -6,13 +6,13 @@ const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".section-3__arrow-right",
+    prevEl: ".section-3__arrow-left",
   },
   // autoHeight: false,
   slidesPerView: 1,
-  slidesPerColumn: 2,
-  slidesPerGroup: 3,
-  slidesPerColumnFill: "row",
+  slidesPerColumn: 1,
+  slidesPerGroup: 1,
+  //slidesPerColumnFill: "row",
   spaceBetween: 20,
 });
