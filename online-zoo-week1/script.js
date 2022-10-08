8736acdf588f562b7a9e34f0468a1ebcb2e03ced
Dzmitry Halavach <kotik1@protonmail.com>
2022-10-08 17:40:16 +0300

    feat: slider in pets section

--- online-zoo-week1/script.js
+++ online-zoo-week1/script.js
@@ -4,12 +4,15 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
 
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
-  //loop: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  autoHeight: false,
-  slidesPerView: 3,
+  // autoHeight: false,
+  slidesPerView: 1,
   slidesPerColumn: 2,
+  slidesPerGroup: 3,
+  slidesPerColumnFill: "row",
+  spaceBetween: 20,
 });
