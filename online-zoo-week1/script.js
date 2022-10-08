7becb26d1e8e5f288f33d15386eb99829678e08e
Dzmitry Halavach <kotik1@protonmail.com>
2022-10-08 19:01:32 +0300

    fix: slider

--- online-zoo-week1/script.js
+++ online-zoo-week1/script.js
@@ -9,10 +9,21 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".section-3__arrow-right",
     prevEl: ".section-3__arrow-left",
   },
-  // autoHeight: false,
-  slidesPerView: 1,
-  slidesPerColumn: 1,
-  slidesPerGroup: 1,
-  //slidesPerColumnFill: "row",
+  autoHeight: false,
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  grid: {
+    rows: 2,
+  },
+
   spaceBetween: 20,
+  breakpoints: {
+    640: {
+      spaceBetween: 10,
+    },
+
+    320: {
+      spaceBetween: 5,
+    },
+  },
 });
